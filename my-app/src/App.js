f5712d14d4ed5da709427b7cd20216a4e5b660b3
Sebastian Brown <108308812+Sebastian-Brown@users.noreply.github.com>
2022-10-08 23:33:37 -0500

    Initial Commit
    Obtaining React boiler code and testing components.

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+
+
+function App() {
+  return (
+    <div >
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Test.js
@@ -0,0 +1,12 @@
+const Test = () => {
+  return (
+    <div>
+        <p className="tester">Test</p>
+        <div className="inputContainer">
+        <h1>URL <span>Shortener</span></h1>
+        </div>
+    </div>
+  )
+}
+
+export default Test
