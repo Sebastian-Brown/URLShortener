6798e6ae049adac8a6e7ef2cbe8574d37debb996
Striddy2 <striddy2@yahoo.com>
2022-11-17 17:10:50 -0600

    server config

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+mongoose.connect('mongodb://localhost/URLShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.get('/',(req, res) => {
+    res.send("hell world");
+})
+
+app.post('/shortUrls', (req, res) => {
+
+})
+
+app.listen(process.env.PORT || 4000)
